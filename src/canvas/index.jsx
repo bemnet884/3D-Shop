5361ab053d6af3e215b03aaefb12bc902f435ec2
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-19 18:17:38 -0700

    change

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,8 +1,19 @@
 import { Canvas } from '@react-three/fiber'
-import {Environment,Center } from '@react-three/drei'
+import { Environment, Center } from '@react-three/drei'
+import Shirt from './Shirt'
+import BackDrop from './BackDrop'
+import CameraRig from './CameraRig'
+
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      
+      <CameraRig>
+        <BackDrop/>
+      </CameraRig>
+    </Canvas>
   )
 }
 
