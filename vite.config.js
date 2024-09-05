85ec8f62e5549dd9ace57a2be9218200766c5423
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-09-05 12:37:55 -0700

    change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+  base: "/3D-Shop/", // YOUR REPO NAME HERE
 });
